3262201e1da7119278e0182009fa27ff60f977bf
tnjensen <thomasnorupjensen@gmail.com>
2022-05-10 11:36:44 +0200

    Adjusted blog-post-detail layout

--- js/blog-details.js
+++ js/blog-details.js
@@ -1,4 +1,4 @@
-const blogPost = document.querySelector('.blog-post');
+const blogPost = document.querySelector('.blog-post-detail');
 const url = 'https://noroffcors.herokuapp.com/https://noroff.tnjensen.com/blogsite_exam1/wp-json/wp/v2/posts/';
 const loader = document.querySelector('.loader');
 const queryString = document.location.search;
@@ -14,7 +14,8 @@ async function getPost(){
         console.log(result);
         
         blogPost.innerHTML = `<h3>${result.title.rendered}</h3>
-                        <img src="${result._embedded['wp:featuredmedia'][0].source_url}" alt="${result._embedded['wp:featuredmedia'][0].alt_text}" />
+                        <img src="${result._embedded['wp:featuredmedia'][0].source_url}" 
+                            alt="${result._embedded['wp:featuredmedia'][0].alt_text}" />
                         <p>${result.content.rendered}</p>`;
     
     }

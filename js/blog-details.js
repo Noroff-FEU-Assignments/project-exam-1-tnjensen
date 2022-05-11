8ea7b85464f0bd2239b06d9411da511d1536c184
tnjensen <thomasnorupjensen@gmail.com>
2022-05-11 13:33:13 +0200

    Edited modal image code

--- js/blog-details.js
+++ js/blog-details.js
@@ -16,7 +16,7 @@ async function getPost(){
         console.log(result);
         
         blogPost.innerHTML = `<h3>${result.title.rendered}</h3>
-                        <img id="imgFull" onclick="getFeaturedImageLarge();" src="${result._embedded['wp:featuredmedia'][0].source_url}" 
+                        <img id="imgFull" src="${result._embedded['wp:featuredmedia'][0].source_url}" 
                             alt="${result._embedded['wp:featuredmedia'][0].alt_text}" /></a>
                         <p>${result.content.rendered}</p>`;
     
@@ -24,26 +24,24 @@ async function getPost(){
     catch(error){
         blogPost.innerHTML = `Error: ` + error;
     }
+    let imgFull = document.getElementById('imgFull');
+    imgFull.onclick = getFeaturedImageLarge;
 }
 getPost();
 
 function getFeaturedImageLarge(){
-    let imgFull = document.getElementById('imgFull');
-    console.log(imgFull);
     let imgModal = document.getElementById('imgModal');
     let captiontext = document.getElementById('caption');
-    imgFull.addEventListener('click', function(){
-        modal.style.display = "block";
-        imgModal.src = this.src;
-        captiontext.innerHTML = this.alt;
-    })
+    modal.style.display = "block";
+    imgModal.src = this.src;
+    captiontext.innerHTML = this.alt;
 }
 
-const span = document.getElementsByClassName('close')[0];
+/* const span = document.getElementsByClassName('close')[0];
 
 span.onclick = function(){
     modal.style.display = "none";
-}
+} */
 
 window.addEventListener('mouseup', function(event){
     if(!event.target.closest('footer')){

50ef5c4cc9dcb97935befcfc756ce02d6bfe82fb
tnjensen <thomasnorupjensen@gmail.com>
2022-05-20 11:56:08 +0200

    Edited script more posts

--- js/blog.js
+++ js/blog.js
@@ -53,7 +53,7 @@ async function getMorePosts(){
             let blogLink =`<a href="blog-detail.html?id=${results[i].id}" class="cta-small">Read more..</a>`; 
             blogPosts.innerHTML += `<div class="blog-post">
                             <h3>${results[i].title.rendered}</h3>
-                            <img src="${results[i]._embedded['wp:featuredmedia'][0].source_url}" alt="${results[i]._embedded['wp:featuredmedia'][0].alt_text}" />
+                            <a href="blog-detail.html?id=${results[i].id}"><img src="${results[i]._embedded['wp:featuredmedia'][0].source_url}" alt="${results[i]._embedded['wp:featuredmedia'][0].alt_text}" /></a>
                             <p>${results[i].excerpt.rendered}</p>
                             <p>${blogLink}</p>
                             </div>`;
